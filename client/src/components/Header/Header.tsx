c2241ac498bff78972eaa2e2d2f1918c2974bd61
Aditi Pandey <aditipandey@Aditis-MacBook-Air.local>
2026-09-04 18:02:27 +0530

    feat(frontend): add React setup

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import './Header.css';
+
+const Header =()=>{
+    return(
+             <header className="header">
+        <div className="logo">Aivar</div>
+      </header>
+    )
+}
+export default Header;
